91a5f38e3fb38ab4f3ea5e2cc3455bb47f214eb4
hariehkr045 <hariehkr045@gmail.com>
2026-05-25 19:10:46 +0530

    data ingestion pipeline added

--- setup.py
+++ setup.py
@@ -1,3 +1,24 @@
 from setuptools import find_packages, setup
 
-setup()
+
+HYPEN_E_DOT = '-e .'
+def get_requirements(file_path:str)-> list[str]:
+    """This function will retuen the list of requirement"""
+
+    requirements=[]
+    with open(file_path) as file_objs:
+        requirements = file_objs.readlines()
+        requirements = [req.replace('\n')  for req in requirements]
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)     
+
+    return requirements
+
+setup(name="mlproject",
+author ="harikishan",
+author_email="hariehkr_gmail.com",
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt'),
+
+)
+
